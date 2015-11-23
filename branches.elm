9106c1f13114907870bacf2beb43e76d7be70983
Jade Eklund <jade.eklund@gmail.com>
2015-11-23 14:30:47 -0800

    Aum

--- branches.elm
+++ branches.elm
@@ -1,11 +1,23 @@
 --Animated branching fractals that grow to make you feel good.
 --Be one with nature. Experience your physical form. Branches.
 
+import Graphics.Element exposing (..)
+
 --MODEL (SOUTH)
+type alias Model = Int
+
+init : Model
+init = 1
 
 --UPDATE (WEST)
 
+update : Model -> Model
+update model =
+  model + 1 
+
 --DISPLAY (NORTH)
+
+
 
 --MAIN (EAST)
 
