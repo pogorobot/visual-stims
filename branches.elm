81b0771a3746c380292cd04d3ecbeb7739037f09
Jade Eklund <jade.eklund@gmail.com>
2015-11-23 14:00:10 -0800

    AUM

---
+++ branches.elm
@@ -0,0 +1,13 @@
+--Animated branching fractals that grow to make you feel good.
+--Be one with nature. Experience your physical form. Branches.
+
+--MODEL (SOUTH)
+
+--UPDATE (WEST)
+
+--DISPLAY (NORTH)
+
+--MAIN (EAST)
+
+--(UNTIL THE EAST SHALL RISE)
+--(AND ILLUMINATE ALL BEINGS)
