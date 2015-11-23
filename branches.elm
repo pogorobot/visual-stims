be287f254dbf58c29b5e5b62dddc18e9d6f0fa54
Jade Eklund <jade.eklund@gmail.com>
2015-11-23 15:47:00 -0800

    aum

--- branches.elm
+++ branches.elm
@@ -2,6 +2,11 @@
 --Be one with nature. Experience your physical form. Branches.
 
 import Graphics.Element exposing (..)
+import Graphics.Collage exposing (..)
+import Color exposing (..)
+import Time exposing (..)
+import Signal exposing (..)
+import Mouse exposing (..)
 
 --MODEL (SOUTH)
 type alias Model = Int
@@ -11,15 +16,51 @@ init = 1
 
 --UPDATE (WEST)
 
-update : Model -> Model
-update model =
+type Action = Tick 
+  | Tock
+  | NoOp
+
+update : Action -> Model -> Model
+update action model =
   model + 1 
 
---DISPLAY (NORTH)
+actionMailbox : Mailbox Action
+actionMailbox =
+  Signal.mailbox NoOp
 
+address : Address Action
+address =
+  actionMailbox.address
+
+actions : Signal Action
+actions =
+  actionMailbox.signal
+
+--VIEW (NORTH)
+
+view : Address Action -> Model -> Element
+view address model =
+  collage 271 137 [filled  (colorOf model) (circle (sizeOf model))]
+
+colorOf : Model -> Color
+colorOf model =
+  rgb 
+    (255 - model)
+    (floor (255 / toFloat model))
+    (255 % model)
+
+sizeOf : Model -> Float
+sizeOf model =
+  toFloat model * 73.1
 
 
 --MAIN (EAST)
+
+main : Signal Element
+main =
+  Signal.map (view address)
+    (Signal.foldp update init actions) 
+
 
 --(UNTIL THE EAST SHALL RISE)
 --(AND ILLUMINATE ALL BEINGS)
